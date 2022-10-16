44a7e6fa19a1bf5d618ae3b0b6d7ccf30da01547
Miss_Supriya <52968975+khssupriya@users.noreply.github.com>
2022-10-17 00:14:36 +0530

    file changes

--- app.py
+++ app.py
@@ -16,58 +16,39 @@ import pickle
 import pandas as pd
 import streamlit as st 
 
+
+from sklearn.base import BaseEstimator, TransformerMixin
+
+class InputTransformer(BaseEstimator, TransformerMixin):
+    def __init__(self, vectorizer):
+        self.vectorizer = vectorizer
+        print("initalized InputTransformer")
+
+    def fit(self, X, y=None):
+        print('fit')
+        return self
+
+    def transform(self, X, y=None):
+        X_ = X.copy()
+        print('trasform')
+        transformedX = self.vectorizer.transform(X_)
+        return transformedX
+
 pickle_in = open("classifier.pkl","rb")
 classifier=pickle.load(pickle_in)
 
-def predict(variance,skewness,curtosis,entropy):
-    
-    """Let's Authenticate the Banks Note 
-    This is using docstrings for specifications.
-    ---
-    parameters:  
-      - name: variance
-        in: query
-        type: number
-        required: true
-      - name: skewness
-        in: query
-        type: number
-        required: true
-      - name: curtosis
-        in: query
-        type: number
-        required: true
-      - name: entropy
-        in: query
-        type: number
-        required: true
-    responses:
-        200:
-            description: The output values
-        
-    """
-   
-    prediction=classifier.predict([[variance,skewness,curtosis,entropy]])
+def predict(text):
+    prediction=classifier.predict([text])
     print(prediction)
     return prediction
 
-# change
-
 def main():
     st.title("Fake News Classifier")
-    # html_temp = """
-    # <div style="background-color:tomato;padding:10px">
-    # <h2 style="color:white;text-align:center;">Streamlit Bank Authenticator ML App </h2>
-    # </div>
-    # """
-    # st.markdown(html_temp,unsafe_allow_html=True)
-    variance = st.text_input("Variance")
-    skewness = st.text_input("skewness")
-    curtosis = st.text_input("curtosis")
-    entropy = st.text_input("entropy")
+    text = st.text_area("Text", height=200)
+    
     result=""
     if st.button("Predict"):
-        result=predict(variance,skewness,curtosis,entropy)
+        result=predict(text)
         if int(result) == 1:
             st.error('The news is Fake')
         else:
